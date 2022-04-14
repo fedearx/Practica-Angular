08af6e3856a3f65385384cbdcc3fba0ee865c041
Federico A. Rubio <fedearx@gmail.com>
2022-04-14 00:07:20 -0300

    Retocando presentación

--- Ejercicio-1/ejemplo1/store/src/app/app.module.ts
+++ Ejercicio-1/ejemplo1/store/src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [             // injectamos componentes o directivas
+    AppComponent, 
+    HeaderComponent
   ],
-  imports: [
+  imports: [                  // modulos de angular que injectemos
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    MaterialModule,
+    BrowserAnimationsModule,
+    HttpClientModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [],    
+  bootstrap: [AppComponent]   // componente principal
+  // exports: [], componentes que queremos exponer en otros modulos
 })
 export class AppModule { }
---
+++ Ejercicio-1/ejemplo1/store/src/app/pages/products/interfaces/product.interface.ts
@@ -0,0 +1,8 @@
+export interface Product {
+  id: number;
+  name: string;
+  price: number;
+  description: string;
+  categoryID: number;
+  stock: number;
+}
